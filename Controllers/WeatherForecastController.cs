a0d6b6ee2d5272f603e0a58f9c82ee2555c7ad6f
Mohamed Fazrin <mfmfazrin1986@gmail.com>
2022-11-10 13:34:31 +0300

    Commit changes

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -1,9 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
+
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
